c1fdd3f557493b2857c8165f89c3995a71494bd2
dhohirpradana <dhohirkudus@gmail.com>
2022-05-14 08:08:39 +0700

    update

--- lib/widgets/substr.dart
+++ lib/widgets/substr.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-Widget substr(label, data) => Text(
-      label +
-          data.toString().substring(
-                0,
-                (data.toString().length > 5 ? 5 : data.toString().length),
+Widget substr(label, data) => Padding(
+      padding: const EdgeInsets.only(left: 14.0),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              SizedBox(
+                width: Get.width / 3,
+                child: Text(
+                  label,
+                  style: const TextStyle(fontSize: 18),
+                ),
               ),
+              Text(
+                data.toString().substring(
+                      0,
+                      (data.toString().length > 5 ? 5 : data.toString().length),
+                    ),
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+              ),
+            ],
+          ),
+          const Divider(),
+        ],
+      ),
     );
